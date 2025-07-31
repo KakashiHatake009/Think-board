837d3931b146537dda7f9faab245f6c117d8e3de
KakashiHatake009 <prashanth.kuntlapalli@gmail.com>
2025-07-31 17:24:36 +0200

    cros policy update url

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://think-board-rho.vercel.app/",
     })
 );
 
